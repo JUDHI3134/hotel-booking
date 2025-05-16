8bad61858ddf51625a77776fdbd38b5083bde333
JUDHI3134 <judhistirbehera456@gmail.com>
2025-05-16 17:33:06 +0530

    create clerk webhook api endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import "dotenv/config"
 import cors from "cors"
 import connectDB from "./config/db.js"
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js"
 
 
 //database connection
@@ -9,6 +11,12 @@ connectDB()
 
 const app = express()
 app.use(cors())
+
+app.use(express.json())
+app.use(clerkMiddleware())
+
+//API to listen clerk webhooks
+app.use("/api/clerk", clerkWebhooks)
 
 app.get("/", (req, res) => res.send("Api is Working"))
 
