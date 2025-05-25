90e09ba142424c8a784deba1e86a0afef7ef31ed
JUDHI3134 <judhistirbehera456@gmail.com>
2025-05-25 21:06:10 +0530

    create context file in frontend

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,33 @@
+import axios from "axios"
+import { createContext, use, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom"
+import { useUser, useAuth } from "@clerk/clerk-react"
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+
+    const currency = import.meta.env.VITE_CURRENCY || "$"
+    const navigate = useNavigate()
+    const { user } = useUser()
+    const { getToken } = useAuth()
+    
+    const [isOwner, setIsOwner] = useState(false)
+    const [showHotelReg, setShowHotelReg] = useState(false)
+
+    const value = {
+        currency, navigate, user, getToken, isOwner, setIsOwner, axios, showHotelReg, setShowHotelReg
+    }
+    
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => useContext(AppContext)
