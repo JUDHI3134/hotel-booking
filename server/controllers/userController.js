cbf43a82bd4c39e6942c9f42ba4e69aa636df9c5
JUDHI3134 <judhistirbehera456@gmail.com>
2025-05-31 20:50:00 +0530

    changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,7 +5,7 @@ export const getUserData = async (req, res) => {
     try {
         const role = req.user.role
         const recentSearchedCities = req.user.recentSearchedCities 
-        res.json({success: true,role, recentSearchedCities})
+        res.json({success: true, role, recentSearchedCities})
     } catch (error) {
         res.json({success: false, message: error.message})
     }
@@ -14,14 +14,14 @@ export const getUserData = async (req, res) => {
 //store user recent searched cities
 export const storeRecentSearchedCities = async (req, res) => {
     try {
-        const { recentSearchedCitity } = req.body;
+        const { recentSearchedCity } = req.body;
         const user = await req.user;
 
         if (user.recentSearchedCities.length < 3) {
-            user.recentSearchedCities.push(recentSearchedCitity)
+            user.recentSearchedCities.push(recentSearchedCity)
         } else {
             user.recentSearchedCities.shift()
-             user.recentSearchedCities.push(recentSearchedCitity)
+             user.recentSearchedCities.push(recentSearchedCity)
         }
 
         await user.save()
